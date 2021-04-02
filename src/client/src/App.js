3ece397f587dff68a605ff253f5506fc8a06757a
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-04-02 23:02:37 +0530

    Add : First api call

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const App = () => {
-    return <>Hello</>
+    const [data, setData] = useState();
+    useEffect(() => {
+        fetch("/students").then(res => res.json().then(students => setData(students)));
+    }, [])
+
+    return data ? data.map(student => {
+        const {studentId, name, email, gender} = student;
+        return <div key={studentId}>
+            <p> Student Id is : {studentId}</p>
+            <p>Name is : {name}</p>
+            <p>Email is : {email}</p>
+            <p>Gender is : {gender}</p>
+            <br/>
+        </div>
+    }) : <p>Loading</p>
 }
 
 export default App;
