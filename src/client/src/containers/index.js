0d217b15438356213ed85b7b9840a300591678ec
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-04-02 23:56:20 +0530

    Add : Custom container

---
+++ src/client/src/containers/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Container = (props) => {
+  return (
+    <div
+      style={{
+        width: '1400px',
+        margin: '0 auto',
+      }}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export default Container
