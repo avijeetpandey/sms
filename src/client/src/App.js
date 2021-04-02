737cc3be0ff7b243aaadd3d3d6609a5436e817bb
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-04-02 23:51:23 +0530

    Add : Student component

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,21 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React from 'react'
+import Student from './components/Student'
 
 const App = () => {
-    const [data, setData] = useState();
-    useEffect(() => {
-        fetch("/students").then(res => res.json().then(students => setData(students)));
-    }, [])
-
-    return data ? data.map(student => {
-        const {studentId, name, email, gender} = student;
-        return <div key={studentId}>
-            <p> Student Id is : {studentId}</p>
-            <p>Name is : {name}</p>
-            <p>Email is : {email}</p>
-            <p>Gender is : {gender}</p>
-            <br/>
-        </div>
-    }) : <p>Loading</p>
+  return <Student />
 }
 
-export default App;
+export default App
